f028f5726540187df2cf989bd792374e93c061ef
manisha28nagarkoti <126867552+manisha28nagarkoti@users.noreply.github.com>
2023-04-28 12:39:20 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -3,5 +3,6 @@ def call(){
     sudo aws ecr get-login-password --region "${param.region}" | sudo docker login --username AWS --password-stdin "${param.ecr_url}"
     sudo docker tag "${param.image_name}":"${param.tag}" "${param.repo_url}":"${param.tag}"
     sudo docker push "${param.ecr_url}"/"${param.repo_name}":${param.tag}
+    echo "$info"
     '''
 }
